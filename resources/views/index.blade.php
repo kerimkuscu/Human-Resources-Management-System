b1db0893479486bc1c542efbc18faad64764d488
Kerim <kerimkuscu95@gmail.com>
2019-04-15 16:53:10 +0300

    refactor: index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+@php
+    $config = [
+        'appName' => config('app.name'),
+        'locale' => $locale = app()->getLocale(),
+        'locales' => config('app.locales'),
+        'githubAuth' => config('services.github.client_id'),
+    ];
+
+    $polyfills = [
+        'Promise',
+        'Object.assign',
+        'Object.values',
+        'Array.prototype.find',
+        'Array.prototype.findIndex',
+        'Array.prototype.includes',
+        'String.prototype.includes',
+        'String.prototype.startsWith',
+        'String.prototype.endsWith',
+    ];
+@endphp
+        <!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>{{ config('app.name') }}</title>
+    <link rel="shortcut icon" href="https://www.performly.com/assets/images/Intra/hrServices.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @if(config('app.dark_theme'))
+        <link rel="stylesheet" href="{{ mix('css/theme.css') }}">
+    @endif
+</head>
+<body>
+<div id="app"></div>
+
+{{-- Global configuration object --}}
+<script>window.config = @json($config);</script>
+
+{{-- Polyfill JS features via polyfill.io --}}
+<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features={{ implode(',', $polyfills) }}"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+{{-- Load the application scripts --}}
+@if (app()->isLocal())
+    <script src="{{ mix('js/app.js') }}"></script>
+@else
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+@endif
+</body>
+</html>
