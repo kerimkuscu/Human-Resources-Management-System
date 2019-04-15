8f9db80b58ba78c768f0391b3e667dc5360b8c65
Kerim <kerimkuscu95@gmail.com>
2019-04-15 16:31:03 +0300

    refactor: add employee migration

---
+++ database/migrations/2019_03_13_145230_create_employee_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('employees', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->char('nationality', 2)->nullable();
+            $table->dateTime('birthday');
+            $table->string('gender')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->string('driver_license')->nullable();
+            $table->string('employment_status')->nullable();
+            $table->string('job_title');
+            $table->text('address')->nullable();
+            $table->text('city')->nullable();
+            $table->char('country', 2)->nullable();
+            $table->text('province')->nullable();
+            $table->text('postal_code')->nullable();
+            $table->string('home_phone')->nullable();
+            $table->string('mobile_phone')->nullable();
+            $table->string('work_phone')->nullable();
+            $table->string('emergency_contact');
+            $table->string('work_email')->unique()->nullable();
+            $table->string('private_email')->unique()->nullable();
+            $table->dateTime('joined_date');
+            $table->string('department');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
