515cc2813a18a1bade3fa6498959d860876d9394
Kerim <kerimkuscu95@gmail.com>
2019-04-15 16:36:29 +0300

    refactor: add home file for cards

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h4 class="card-title">Home</h4>
+        <hr>
+
+        <div class="row">
+            <div class="col-3">
+                <peoples-card></peoples-card>
+            </div>
+
+            <div class="col-3">
+                <users-card></users-card>
+            </div>
+
+            <div class="col-3">
+                <leaves-card></leaves-card>
+            </div>
+
+            <div class="col-3">
+                <attendance-card></attendance-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import PeoplesCard from './Dashboard/Cards/PeoplesCard';
+    import UsersCard from './Dashboard/Cards/UsersCard';
+    import LeavesCard from './Dashboard/Cards/LeavesCard';
+    import AttendanceCard from './Dashboard/Cards/AttendanceCard';
+
+    export default {
+        components: {
+            PeoplesCard,
+            UsersCard,
+            LeavesCard,
+            AttendanceCard
+        }
+    }
+</script>
