5635e264ab6f052990050301402992b1c1fad5cf
Kerim <kerimkuscu95@gmail.com>
2019-04-15 16:31:13 +0300

    refactor: add attendance migration

---
+++ database/migrations/2019_04_12_112233_create_attendance_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('attendances', function(Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('employee_id')->nullable();
+            $table->dateTime('time_in_date');
+            $table->unsignedInteger('time_in_time_hour');
+            $table->unsignedInteger('time_in_time_minute');
+            $table->dateTime('time_out_date');
+            $table->unsignedInteger('time_out_time_hour');
+            $table->unsignedInteger('time_out_time_minute');
+            $table->text('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
