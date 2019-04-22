088a5e84de5a12f076e9ae5532a43c77d9b6f18e
Kerim <kerimkuscu95@gmail.com>
2019-04-22 21:18:24 +0300

    refactor: add documentation form request

--- app/Http/Requests/DocumentationFormRequest.php
+++ app/Http/Requests/DocumentationFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRoleFormRequest extends FormRequest
+class DocumentationFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class UserRoleFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'users_role' => 'required|min:5',
+            //
         ];
     }
 }
