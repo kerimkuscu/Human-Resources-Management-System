c13df0925e42b3e1a6db6eb8fa793456e637d1e1
Kerim <kerimkuscu95@gmail.com>
2019-04-22 21:24:54 +0300

    refactor: using clear code automation

--- app/Models/UserLeave.php
+++ app/Models/UserLeave.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\BelongsToUser;
+use Barryvdh\LaravelIdeHelper\Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,17 +12,17 @@ use Illuminate\Support\Carbon;
 /**
  * App\Models\UserLeave
  *
- * @property int         $id
- * @property int         $user_id
- * @property int         $leave_type_id
- * @property Carbon      $started_at
- * @property Carbon      $ended_at
- * @property string      $description
- * @property int|null    $approved_by
- * @property string|null $approved_at
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property-read User   $user
+ * @property int            $id
+ * @property int            $user_id
+ * @property int            $leave_type_id
+ * @property Carbon         $started_at
+ * @property Carbon         $ended_at
+ * @property string         $description
+ * @property int|null       $approved_by
+ * @property string|null    $approved_at
+ * @property Carbon|null    $created_at
+ * @property Carbon|null    $updated_at
+ * @property-read User      $user
  * @method static Builder|UserLeave newModelQuery()
  * @method static Builder|UserLeave newQuery()
  * @method static Builder|UserLeave query()
@@ -35,9 +36,10 @@ use Illuminate\Support\Carbon;
  * @method static Builder|UserLeave whereStartedAt($value)
  * @method static Builder|UserLeave whereUpdatedAt($value)
  * @method static Builder|UserLeave whereUserId($value)
+ * @method belongsTo(string $class, string $string)
  * @mixins \Eloquent
  * @property-read User|null $approver
- * @mixin \Eloquent
+ * @mixin Eloquent
  */
 class UserLeave extends Model
 {
