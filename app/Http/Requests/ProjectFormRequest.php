456b4374c0c6a0bd3f519253a41c169b80820de2
Kerim <kerimkuscu95@gmail.com>
2019-04-22 21:23:22 +0300

    refactor: add project form request for project form validation

---
+++ app/Http/Requests/ProjectFormRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'project_name' => 'required|min:5',
+            'details'      => 'required|min:5',
+            'status'       => 'required',
+        ];
+    }
+}
