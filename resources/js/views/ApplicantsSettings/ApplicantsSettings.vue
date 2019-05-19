b86e6c9cc4ca6beaf5f8b676f91203fd533ad43b
kerimkuscu <kerimkuscu95@gmail.com>
2019-05-20 00:58:46 +0300

    refactor: add applicants settings page

---
+++ resources/js/views/ApplicantsSettings/ApplicantsSettings.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h4 class="card-title">Applicants Settings</h4>
+
+        <hr>
+
+        <div id="applicantsSettingsTab">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <router-link :to="{ name:'applicants.candidates-list'}" class="nav-link">Candidates List</router-link>
+                </li>
+            </ul>
+        </div>
+
+        <div id="applicantsSettingsTabContent" class="tab-content clearfix">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
