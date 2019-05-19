a8fbab561b08b6a21014256965fe9db7274b7172
kerimkuscu <kerimkuscu95@gmail.com>
2019-05-20 01:00:28 +0300

    refactor: add candidates list

---
+++ resources/js/views/ApplicantsSettings/Candidates/CandidatesList.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+
+        <div class="loading" v-if="loading">
+            Loading...
+        </div>
+
+        <div v-if="errors" class="error">
+            {{ errors }}
+        </div>
+
+        <hr>
+
+        <router-link :to="{ name:'applicants.candidates-form' }">
+            <button type="button" class="btn btn-success btn-sm float-right" data-tooltip="true" title="Candidates Form"><i class="fas fa-plus"></i></button>
+        </router-link>
+
+        <h4 class="card-title">Candidates List</h4>
+
+        <hr>
+
+        <div>
+            <data-tables-server :data="items" :total="total" :pagination-props="{ background: true, pageSizes: [5, 10, 20] }" :action-col="actionCol">
+                <el-table-column v-for="title in titles" :prop="title.prop" :label="title.label" :key="title.label"></el-table-column>
+            </data-tables-server>
+        </div>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: (self = this) => ({
+            loading: false,
+            items: null,
+            errors: null,
+            total: null,
+
+            titles: [],
+            actionCol: {
+                label: 'Actions',
+                props: {
+                    align: 'center',
+                },
+                buttons: [{
+                    props: {
+                        type: 'primary',
+                        icon: 'fas fa-edit',
+                        size: 'mini',
+                    },
+                    handler: (row) => {
+                        self.$router.push({name: 'applicants.candidates-edit', params: {id: row.id}})
+                    }
+                }, {
+                    props: {
+                        type: 'danger',
+                        icon: 'fas fa-trash',
+                        size: 'mini',
+                    },
+                    handler: (row) => {
+                        self.$router.push({name: 'applicants.candidates-delete', params: {id: row.id}})
+                    }
+                }]
+            }
+        }),
+
+        mounted() {
+            this.fetch();
+            this.getTitles();
+        },
+
+        methods: {
+
+            fetch() {
+                this.error = this.items = null;
+
+                this.loading = true;
+
+                this.$http.get('/api/users/candidates')
+                    .then(response => {
+                        this.items = response.data.data;
+                        this.total = this.items.length;
+                    }).catch(error => {
+                    this.error = error.response.data.message || error.message;
+                }).finally(() => {
+                    this.loading = false;
+                })
+            },
+
+            getTitles() {
+                this.error = this.items = null;
+
+                this.loading = true;
+
+                this.$http.get('/api/users/candidates-title')
+                    .then(response => {
+                        this.titles = response.data;
+                    }).catch(error => {
+                    this.error = error.response.data.message || error.message;
+                }).finally(() => {
+                    this.loading = false;
+                });
+            }
+        },
+    }
+</script>
