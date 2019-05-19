5406aa3f9d0306ee139331a03595e8b4f4a54636
kerimkuscu <kerimkuscu95@gmail.com>
2019-05-20 00:59:43 +0300

    refactor: change login page

--- resources/js/pages/AuthLogin.vue
+++ resources/js/pages/AuthLogin.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="container">
         <div class="col-md-6 mx-auto">
-            <div class="card dark-theme-border">
-                <h5 class="card-header">Login</h5>
+            <div class="card login">
+
+                <div class="card-header">
+                    <h1 class="text-center">HRMS</h1>
+                </div>
 
                 <div class="card-body">
                     <div class="alert alert-danger" v-if="errors.has('error')">
@@ -10,27 +13,35 @@
                     </div>
 
                     <form autocomplete="off" @submit.prevent="login" method="post">
-                        <div class="form-group">
-                            <label for="email" class="col-form-label required">E-mail</label>
-                            <input type="email" id="email" class="form-control" :class="{ 'is-invalid': errors.has('email') }" placeholder="user@example.com" v-model="email">
+                        <div class="form-group row" style="padding: 15px">
+                            <label for="email" class="col-1 col-form-label"><i class="fas fa-user fa-2x"></i></label>
+                            <div class="col-11">
+                                <input type="email" id="email" class="form-control" :class="{ 'is-invalid': errors.has('email') }" placeholder="user@example.com" v-model="email">
+                            </div>
 
                             <span class="invalid-feedback">{{ errors.first('email') }}</span>
                         </div>
 
-                        <div class="form-group">
-                            <label for="password" class="col-form-label required">Password</label>
-                            <input type="password" id="password" class="form-control" :class="{ 'is-invalid': errors.has('password') }" v-model="password">
+                        <hr>
+
+                        <div class="form-group row" style="padding: 15px">
+                            <label for="password" class="col-1 col-form-label"><i class="fas fa-lock fa-2x"></i></label>
+                            <div class="col-11">
+                                <input type="password" id="password" class="form-control" :class="{ 'is-invalid': errors.has('password') }" v-model="password">
+                            </div>
 
                             <span class="invalid-feedback">{{ errors.first('password') }}</span>
                         </div>
+
+                        <hr>
 
                         <div class="form-check">
                             <input type="checkbox" class="form-check-input" v-model="remember_me" :true-value="1" :false-value="0">
                             <label class="form-check-label"> Remember Me</label>
                         </div>
 
-                        <div class="float-right">
-                            <button type="submit" class="btn btn-primary">Login</button>
+                        <div class="text-center" style="padding: 15px">
+                            <button type="submit" class="btn btn-primary" style="padding-right: 100px; padding-left: 100px">Login</button>
                         </div>
                     </form>
                 </div>
