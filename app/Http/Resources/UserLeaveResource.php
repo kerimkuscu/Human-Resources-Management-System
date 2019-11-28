9a031530376343ea99ec68343d48f31148b34159
kerimkuscu <kerimkuscu95@gmail.com>
2019-11-28 23:45:52 +0300

    refactor: add new param

--- app/Http/Resources/UserLeaveResource.php
+++ app/Http/Resources/UserLeaveResource.php
@@ -23,13 +23,14 @@ class UserLeaveResource extends JsonResource
         $model = $this->resource;
 
         return [
-            'id'            => $model->id,
-            'leave_type_id' => $model->leave_type_id,
-            'started_at'    => $model->started_at->toDateString(),
-            'ended_at'      => $model->ended_at->toDateString(),
-            'description'   => $model->description,
-            'approved_at'   => $model->approved_at,
-            'approved_by'   => [
+            'id'              => $model->id,
+            'leave_type_id'   => $model->leave_type_id,
+            'leave_type_name' => $model->user_leave_type->leave_type,
+            'started_at'      => $model->started_at->toDateString(),
+            'ended_at'        => $model->ended_at->toDateString(),
+            'description'     => $model->description,
+            'approved_at'     => $model->approved_at,
+            'approved_by'     => [
                 'name'  => optional($model->approver)->name,
                 'email' => optional($model->approver)->email,
             ],
