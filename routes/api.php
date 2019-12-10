4b53caa75aa579a51b2f3c7605f17e1e98f4723b
kerimkuscu <kerimkuscu95@gmail.com>
2019-12-11 00:51:59 +0300

    refactor: update assets

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,29 @@
 <?php
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CandidatesController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserLeaveController;
 use App\Http\Controllers\UserLeaveTypeController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserRoleController;
 
 Route::group([
     'middleware' => 'api',
     'namespace' => 'Auth',
     'prefix' => 'auth',
 ], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::get('user', 'AuthController@user');
-    Route::post('register', 'RegisterController@register');
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('register', [RegisterController::class, 'register']);
 });
 
 Route::group(['middleware' => ['auth:api', 'json']], function () {
-    Route::get('me', 'UserController@me');
+    Route::get('me', [UserController::class, 'me']);
 
     //Leaves Form
     Route::get('users/leaves', [UserLeaveController::class, 'index']);
@@ -43,42 +37,36 @@ Route::group(['middleware' => ['auth:api', 'json']], function () {
     Route::post('users/leave-type', [UserLeaveTypeController::class, 'store']);
     Route::put('users/leave-type/{id}', [UserLeaveTypeController::class, 'update']);
     Route::delete('users/leave-type/{id}', [UserLeaveTypeController::class, 'destroy']);
-    Route::get('users/leave-type-title', 'UserLeaveTypeController@getTitles');
 
     //Users Role
-    Route::get('users/roles', 'UserRoleController@index');
-    Route::post('users/roles', 'UserRoleController@store');
-    Route::delete('users/roles/{id}', 'UserRoleController@destroy');
-    Route::get('users/roles-title', 'UserRoleController@getTitles');
+    Route::get('users/roles', [UserRoleController::class, 'index']);
+    Route::post('users/roles', [UserRoleController::class, 'store']);
+    Route::delete('users/roles/{id}', [UserRoleController::class, 'destroy']);
 
     //Employees
-    Route::get('users/employees', 'EmployeeController@index');
-    Route::get('users/employees/{id}', 'EmployeeController@show');
-    Route::post('users/employees', 'EmployeeController@store');
-    Route::put('users/employees/{id}', 'EmployeeController@update');
-    Route::delete('users/employees/{id}', 'EmployeeController@destroy');
-    Route::get('users/employees-title', 'EmployeeController@getTitles');
+    Route::get('users/employees', [EmployeeController::class, 'index']);
+    Route::get('users/employees/{id}', [EmployeeController::class, 'show']);
+    Route::post('users/employees', [EmployeeController::class, 'store']);
+    Route::put('users/employees/{id}', [EmployeeController::class, 'update']);
+    Route::delete('users/employees/{id}', [EmployeeController::class, 'destroy']);
 
     //Attendance
-    Route::get('users/attendance', 'AttendanceController@index');
-    Route::get('users/attendance/{id}', 'AttendanceController@show');
-    Route::post('users/attendance', 'AttendanceController@store');
-    Route::put('users/attendance/{id}', 'AttendanceController@update');
-    Route::delete('users/attendance/{id}', 'AttendanceController@destroy');
-    Route::get('users/attendance-title', 'AttendanceController@getTitles');
+    Route::get('users/attendance', [AttendanceController::class, 'index']);
+    Route::get('users/attendance/{id}', [AttendanceController::class, 'show']);
+    Route::post('users/attendance', [AttendanceController::class, 'store']);
+    Route::put('users/attendance/{id}', [AttendanceController::class, 'update']);
+    Route::delete('users/attendance/{id}', [AttendanceController::class, 'destroy']);
 
     //Candidates
-    Route::get('users/candidates', 'CandidatesController@index');
-    Route::get('users/candidates/{id}', 'CandidatesController@show');
-    Route::post('users/candidates', 'CandidatesController@store');
-    Route::put('users/candidates/{id}', 'CandidatesController@update');
-    Route::delete('users/candidates/{id}', 'CandidatesController@destroy');
-    Route::get('users/candidates-title', 'CandidatesController@getTitles');
+    Route::get('users/candidates', [CandidatesController::class, 'index']);
+    Route::get('users/candidates/{id}', [CandidatesController::class, 'show']);
+    Route::post('users/candidates', [CandidatesController::class, 'store']);
+    Route::put('users/candidates/{id}', [CandidatesController::class, 'update']);
+    Route::delete('users/candidates/{id}', [CandidatesController::class, 'destroy']);
 
     //Users
-    Route::get('users/', 'UserController@index');
-    Route::get('users/{id}', 'UserController@show');
-    Route::post('users/{id}', 'UserController@store');
-    Route::put('users/{id}', 'UserController@update');
-    Route::get('users-title/', 'UserController@getTitles');
+    Route::get('users/', [UserController::class, 'index']);
+    Route::get('users/{id}', [UserController::class, 'show']);
+    Route::post('users/{id}', [UserController::class, 'store']);
+    Route::put('users/{id}', [UserController::class, 'update']);
 });
